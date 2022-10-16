58f44105e3764a8ed5ea1130e41ae564d5183265
sena <senaakbas0@gmail..com>
2022-10-16 23:03:01 +0300

    Initial commit

---
+++ src/Selamlar.java
@@ -0,0 +1,6 @@
+public class Selamlar {
+    public static void main(String[] args) {
+        System.out.println("Burak bey ACİL DURUM !!!!!!!!"+
+        "eğer acil durumuöğrenmek isterseniz lütfen kodda değişiklik yapın");
+    }
+}
